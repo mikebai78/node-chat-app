17cf03506cd2ed843cfc8cdb78a8baf279f4c1fa
MB <mb@gmail.com>
2018-06-18 11:15:06 -0600

    add socket.ip to app

--- server/server.js
+++ server/server.js
@@ -1,20 +1,34 @@
 const path = require('path');
-
+const http = require('http');
 const express = require('express');
+const socket = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socket(server);
 
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//   res.send(index.html);
-// })
+app.get('/', (req, res) => {
+res.sendFile('index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  })
+
+
+});
 
 
 
-app.listen(port, () => {
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 })
